99b48d28270f0d28065847e30ff868c75558b9ff
Uneiz Sheikh <uneizsheikh719@gmail.com>
2025-05-19 12:24:43 +0530

    build

--- src/Components/JoinNow.tsx
+++ src/Components/JoinNow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
